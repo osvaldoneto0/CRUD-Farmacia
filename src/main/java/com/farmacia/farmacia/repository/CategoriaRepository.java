ad844c03114114ad28c148bf81c02129cf5e0ee6
osvaldoneto0 <osvaldonetoo011@gmail.com>
2024-06-10 15:27:43 -0300

    CRUD Categoria

--- src/main/java/com/farmacia/farmacia/repository/CategoriaRepository.java
+++ src/main/java/com/farmacia/farmacia/repository/CategoriaRepository.java
@@ -9,7 +9,7 @@ import com.farmacia.farmacia.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria,Long>{
 	
-	public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("categoria") String categoria);
+	public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
 
 }
