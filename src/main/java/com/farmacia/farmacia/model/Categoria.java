62209e1ead1d2266dba0c39f01bcdd64b3741464
osvaldoneto0 <osvaldonetoo011@gmail.com>
2024-06-10 15:13:29 -0300

    Criando o CRUD da Categoria

---
+++ src/main/java/com/farmacia/farmacia/model/Categoria.java
@@ -0,0 +1,39 @@
+package com.farmacia.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O Atributo Nome é obrigatório")
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+
+}
